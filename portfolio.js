e881b2c840270777c92a383ef6761a118a957f0c
Prachi Arora <82777630+prachiarora1@users.noreply.github.com>
2021-04-22 02:04:03 +0530

    Deployment changes

--- portfolio.js
+++ portfolio.js
@@ -3,3 +3,11 @@ function sendEmail()
 {
     window.location = "mailto:prachiarora0111@gmail.com";
 }
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
